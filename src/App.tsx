3789702c4fec43f90c9d5cd6f9253653975c44b3
mkpage1337 <mrku1337@gmail.com>
2024-09-10 19:16:28 +0200

    adding components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Home from "./components/Home";
+import Store from "./components/Store";
+import About from "./components/About";
+
 export default function App() {
   return (
-    <>
-      <h1>hi</h1>
-    </>
-  )
+    <Container>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Container>
+  );
 }
-
