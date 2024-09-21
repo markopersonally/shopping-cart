3102ecda305ed757250480791bd826808aab4a12
mkpage1337 <mrku1337@gmail.com>
2024-09-21 18:46:34 +0200

    styling/home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,15 @@
+import { Container, Button } from "react-bootstrap";
+import img from "../images/philipsburg-2656850_1280.jpg";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <Container className="text-center mt-5">
+      <img src={img} alt="main product" className="rounded img-fluid mb-4" />
+      <div>
+        <Button variant="primary" href="/store" size="lg">
+          Shop Now
+        </Button>
+      </div>
+    </Container>
+  );
 }
